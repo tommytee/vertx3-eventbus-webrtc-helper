ba24ed03fd246b0e82e842f302c6e896c24c3ccd
tommytee <github@nicecomputersny.com>
2017-05-25 01:56:20 +0200

    cleanup

--- index.js
+++ index.js
@@ -7,138 +7,7 @@
   var registeredStatusAddress = {};
   var eb, logger = Java.type( "io.vertx.core.logging.LoggerFactory" ).getLogger( 'vertx3-eventbus-webrtc-helper' );
 
-  module.exports.init = function ( ebus ) {
-
-    eb = ebus;
-
-    eb.consumer( 'webrtc.bridge' ).handler( function ( message ) {
-
-      var msg = message.body();
-
-      if ( msg.sendWithReply ) {
-
-        sendingWithReply( message );
-
-      } else if ( msg.register ) {
-
-        if ( registeredAddress[ msg.address ] ) {
-
-          registeredAddress[ msg.address ].peerClients[ msg.peerId ] = true;
-
-        } else {
-
-          registeredAddress[ msg.address ] = {
-            consumer: eb.consumer( msg.address ),
-            peerClients: {}
-          };
-
-          registeredAddress[ msg.address ].peerClients[ msg.peerId ] = true;
-
-          registeredAddress[ msg.address ].consumer.handler( function ( msg4client ) {
-
-            var body = msg4client.body();
-            var address = msg4client.address();
-            var replyAddress = msg4client.replyAddress();
-
-            var fromPeerId = msg4client.headers().get( "peerId" );
-            var peerString = msg4client.headers().get( "peers" );
-
-            var clients = registeredAddress[ address ].peerClients;
-
-            if ( replyAddress ) {
-
-              replyDealer( msg4client );
-
-            } else {
-
-              if ( fromPeerId ) {
-
-                if ( peerString ) {
-
-                  var thePeers = getPeerIds( peerString );
-
-                  Object.keys( clients ).forEach( function ( peerId ) {
-
-                    if ( thePeers[ peerId ] ) {
-
-                      logger.info( 'not forwarding to ' + peerId )
-
-                    } else {
-
-                      forward( peerId, fromPeerId, address, body );
-
-                    }
-
-                  } );
-
-                } else {
-
-                  Object.keys( clients ).forEach( function ( peerId ) {
-
-                    forward( peerId, fromPeerId, address, body );
-
-                  } );
-
-                }
-
-              } else {
-
-                Object.keys( clients ).forEach( function ( peerId ) {
-                  
-                  eb.send( 'webrtc.' + peerId, { address: address, body: body }, { headers: {} } );
-
-                } );
-
-              }
-            }
-
-          } );
-
-        }
-
-      } else if ( msg.unregister ) {
-
-        logger.info( 'unregister peer ' + msg.peerId + ' from address ' + msg.address );
-
-        if ( registeredAddress[ msg.address ] ) {
-
-          unregPeerFromAddress( msg.address, msg.peerId );
-
-        } else {
-
-          logger.error( 'Error unregister attempt for non-existing address ' + msg.address );
-          
-        }
-
-      } else if ( msg.registerStatus ) {
-
-        logger.info('register status for address ' + msg.address );
-
-        if ( ! registeredStatusAddress[ msg.address ] ) {
-
-          registeredStatusAddress[ msg.address ] = {};
-
-        }
-
-        registeredStatusAddress[ msg.address ][ msg.peerId ] = true;
-
-      } else if ( msg.unregisterStatus ) {
-
-        if ( registeredStatusAddress[ msg.address ] ) {
-
-          unregPeerFromStatusAddress( msg.address, msg.peerId )
-
-          logger.info('unregister status for address ' + msg.address + ', peer: ' + msg.peerId );
-
-        }
-
-      }
-
-    } )
-
-  }
-
-  module.exports.bridgeEvent = function ( be ) {
+  function bridgeEvent( be ) {
 
     var peerId, msg, type = be.type();
 
@@ -157,25 +26,24 @@
         delete clientsByPeerId[ peerId ];
 
         // look through registered addresses for this peer id
+
         Object.keys( registeredAddress ).forEach( function ( address ) {
 
           if ( registeredAddress[ address ].peerClients[ peerId ] ) {
 
             unregPeerFromAddress( address, peerId );
 
-            logger.info( 'unregistered peer ' + peerId + ' from ' + address );
           }
 
         } );
 
         // look through registered status addresses for this peer id
+
         Object.keys( registeredStatusAddress ).forEach( function ( address ) {
 
           if ( registeredStatusAddress[ address ][ peerId ] ) {
 
             unregPeerFromStatusAddress( address, peerId );
-
-            logger.info( 'status unregistered peer ' + peerId + ' from ' + address );
 
           }
 
@@ -198,7 +66,121 @@
     be.complete( true );
 
   }
+  
+  function helperMessage( message ) {
 
+    var msg = message.body();
+
+    if ( msg.sendWithReply ) {
+
+      sendingWithReply( message );
+
+    } else if ( msg.register ) {
+
+      if ( ! registeredAddress[ msg.address ] ) {
+
+        registeredAddress[ msg.address ] = { consumer: eb.consumer( msg.address ), peerClients: {} };
+        registeredAddress[ msg.address ].consumer.handler( addressHandler );
+
+      }
+
+      registeredAddress[ msg.address ].peerClients[ msg.peerId ] = true;
+
+    } else if ( msg.unregister ) {
+
+      if ( registeredAddress[ msg.address ] ) {
+
+        unregPeerFromAddress( msg.address, msg.peerId );
+
+      } else {
+
+        logger.error( 'Error unregister attempt for non-existing address ' + msg.address );
+
+      }
+
+    } else if ( msg.registerStatus ) {
+
+      logger.info('register status for address ' + msg.address );
+
+      if ( ! registeredStatusAddress[ msg.address ] ) {
+
+        registeredStatusAddress[ msg.address ] = {};
+
+      }
+
+      registeredStatusAddress[ msg.address ][ msg.peerId ] = true;
+
+    } else if ( msg.unregisterStatus ) {
+
+      if ( registeredStatusAddress[ msg.address ] ) {
+
+        unregPeerFromStatusAddress( msg.address, msg.peerId )
+
+      }
+
+    }
+
+  }
+
+  function addressHandler ( msg4client ) {
+
+    var body = msg4client.body();
+    var address = msg4client.address();
+    var replyAddress = msg4client.replyAddress();
+    var fromPeerId = msg4client.headers().get( "peerId" );
+    var peerString = msg4client.headers().get( "peers" );
+    var clients = registeredAddress[ address ].peerClients;
+
+    if ( replyAddress ) {
+
+      replyDealer( msg4client );
+
+    } else {
+
+      if ( fromPeerId ) {
+
+        if ( peerString ) {
+
+          var thePeers = getPeerIds( peerString );
+
+          Object.keys( clients ).forEach( function ( peerId ) {
+
+            if ( thePeers[ peerId ] ) {
+
+              logger.info( 'not forwarding to ' + peerId );
+
+            } else {
+
+              forward( peerId, fromPeerId, address, body );
+
+            }
+
+          } );
+
+        } else {
+
+          Object.keys( clients ).forEach( function ( peerId ) {
+
+            forward( peerId, fromPeerId, address, body );
+
+          } );
+
+        }
+
+      } else {
+
+        Object.keys( clients ).forEach( function ( peerId ) {
+
+          eb.send( 'webrtc.' + peerId, { address: address, body: body }, { headers: {} } );
+
+        } );
+
+      }
+
+    }
+
+  }
+  
   function forward ( peerId, fromPeerId, address, body ) {
 
     var envelope = {
@@ -207,6 +189,7 @@
     };
 
     eb.send( 'webrtc.' + peerId, envelope, { headers: { "peerId": fromPeerId } } );
+
   }
 
   function unregPeerFromAddress ( address, peerId ) {
@@ -215,9 +198,12 @@
 
       delete registeredAddress[ address ].peerClients[ peerId ];
 
-      sendStatusUpdate( address, peerId );
+      logger.info( 'unregistered peer ' + peerId + ' from ' + address );
+
+      statusUpdateListenerRemoved( address, peerId );
 
       // if no clients left unregister
+
       if ( Object.keys( registeredAddress[ address ].peerClients ).length === 0 ) {
 
         registeredAddress[ address ].consumer.unregister();
@@ -240,7 +226,10 @@
 
       delete registeredStatusAddress[ address ][ peerId ];
 
+      logger.info('unregister status for address ' + msg.address + ', peer: ' + msg.peerId );
+
       // if no clients left delete registeredStatusAddress
+
       if ( Object.keys( registeredStatusAddress[ address ] ).length === 0 ) {
 
         delete registeredStatusAddress[ address ];
@@ -255,9 +244,9 @@
 
   }
 
-  function sendStatusUpdate ( address, fromPeerId ) {
+  function statusUpdateListenerRemoved ( address, fromPeerId ) {
 
-    //logger.info( 'sendStatusUpdate ' + address );
+    //logger.info( 'statusUpdateListenerRemoved ' + address );
 
     if ( registeredStatusAddress[ address ] ) {
 
@@ -287,6 +276,7 @@
     }
 
     return peers
+
   }
 
   function sendingWithReply ( message ) {
@@ -298,6 +288,7 @@
       onReply( reply, error, message );
 
     } )
+
   }
 
   function onReply ( mes1, err1, message ) {
@@ -324,9 +315,7 @@
   function replyDealer ( msg4client ) {
 
     var address = msg4client.address();
-
     var peerId = Object.keys( registeredAddress[ address ].peerClients )[ 0 ];
-
     var envelope = {
       address: address,
       body: msg4client.body()
@@ -338,6 +327,13 @@
 
     } )
 
+  }
+
+  module.exports.bridgeEvent = bridgeEvent;
+  
+  module.exports.init = function ( ebus ) {
+    eb = ebus;
+    eb.consumer( 'webrtc.bridge' ).handler( helperMessage );
   }
 
 })()
